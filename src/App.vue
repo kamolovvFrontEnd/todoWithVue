e0ec5c7b0b5d2035c980cc2637c60921f134c77b
kamolovv11 <kmlvbilol@gmail.com>
2023-12-16 22:51:59 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Todo App</h1>
+    <form @submit="onSubmitHandler">
+      <input v-model="text" type="text" placeholder="Enter ..." @change="(e) => text = e.target.value"/>
+      <button type="submit" @click="todos.push(text)">Submit</button>
+    </form>
+  </div>
+  <div>
+    <ol>
+      <li v-for="(todo, i) in todos">
+        {{ todo }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+const todos = ref([])
+
+const onSubmitHandler = (e) => {
+  e.preventDefault()
+}
+
+</script>
